3a8b08c30746387505fde819252dc486c3a931d2
LeeTaeKyung <leek9708@gmail.com>
2023-02-24 17:26:39 +0900

    부모 자식 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from "react";
+
+function GrandFather() {
+  return <Mother />;
+}
+function Mother() {
+  return <Son />;
+}
+
+function Son() {
+  return <div> 나는 아들 </div>;
+}
+
 function App() {
-  // <---- 자바스크립트 영역 ---->
-  function clinkBtn() {
-    alert("클릭");
-  }
-  return (
-    /* <---- HTML/JSX 영역  ---->*/
-    <div
-      style={{
-        height: "100vh",
-        display: " flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      {/* 이곳에 퀴즈를 위한 html 코드를 작성해 주세요 */}
-      <span>이것은 내가 만든 App 컴포넌트 입니다.</span>
-      <button onClick={clinkBtn}>클릭!</button>
-    </div>
-  );
+  return <GrandFather />;
 }
 
 export default App;
