29605c84823bf362ac6e17ad8dc3f509a16c1275
LeeTaeKyung <leek9708@gmail.com>
2023-02-24 22:05:35 +0900

    state

--- src/App.js
+++ src/App.js
@@ -8,19 +8,42 @@ function App() {
   // useState, useEffect, useContext, useMemo
   // useDispatch,
   // const [name, setName] = useState("김할아버지");
-  const [fruit, setFruit] = useState("");
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
+
   return (
     <div>
-      과일 :{" "}
-      <input
-        value={fruit}
-        onChange={function (event) {
-          // console.log(event.target.value);
-          setFruit(event.target.value);
+      <form
+        onSubmit={function () {
+          alert(
+            `고객님이 입력하신 아이디는 ${id}이며, 비밀번호는 ${pw} 입니다.`
+          );
+          setId(""); // 초기화
+          setPw(""); // 초기화
         }}
-      />
-      <br />
-      {fruit}
+      >
+        아이디 :{" "}
+        <input
+          type="text"
+          required
+          value={id}
+          onChange={function (event) {
+            // console.log(event.target.value);
+            setId(event.target.value);
+          }}
+        />
+        <br />
+        비밀번호 :{" "}
+        <input
+          type="password"
+          value={pw}
+          required
+          onChange={function (event) {
+            setPw(event.target.value);
+          }}
+        />
+        <br /> <input type="submit" value="로그인" />
+      </form>
     </div>
   );
 }
