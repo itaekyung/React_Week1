77923f510cde1e3f055fbb1b57d2dbb25fd2304b
LeeTaeKyung <leek9708@gmail.com>
2023-02-24 17:44:02 +0900

    jsx 문법

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from "react";
 
-function GrandFather() {
-  return <Mother />;
+export default function App() {
+  const number = 11;
+  const pTagStyle = { color: "red" };
+  return (
+    <div className="test-class">
+      <p style={pTagStyle}> 안녕하세요 리액트입니다.</p>
+      {/* 주석을 사용하는 방법입니다. */}
+      {/* 삼항 연산자를 사용해볼게요! */}
+      <p style={pTagStyle}>
+        {number > 10 ? number + "은 10 보다 크다" : number + " 은 10 보다 작다"}
+      </p>
+    </div>
+  );
 }
-function Mother() {
-  return <Son />;
-}
-
-function Son() {
-  return <div> 나는 아들 </div>;
-}
-
-function App() {
-  return <GrandFather />;
-}
-
-export default App;
