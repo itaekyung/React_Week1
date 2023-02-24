c4a8383af723f9212e4eb5aa918aef60f4e89038
LeeTaeKyung <leek9708@gmail.com>
2023-02-24 20:35:30 +0900

    state onClick

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React, { Children } from "react";
-import Child from "./Child";
+import React, { useState } from "react";
 
 function App() {
-  const name = "test";
-  return <Child name={name}>이름</Child>;
+  // hook , 리액트에서 제공하는 함수
+  // const [state, setState] = useState("초기값");
+  // const [count, setCount] = useState(0);
+  // const [todoList, setTodoList] = useState([]);
+  // useState, useEffect, useContext, useMemo
+  // useDispatch,
+  const [name, setName] = useState("김할아버지");
+
+  return (
+    <div>
+      {name}
+      <br />
+      <button
+        onClick={function () {
+          setName("박할아버지");
+        }}
+      >
+        클릭
+      </button>
+    </div>
+  );
 }
 
 export default App;
