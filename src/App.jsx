05b12009cce7d6ce3023f07739c4bcb202fc0c41
LeeTaeKyung <leek9708@gmail.com>
2023-02-28 20:15:42 +0900

    count

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,24 @@
 import React, { useState } from "react";
 
 function App() {
-  const [id, setId] = useState("");
-  const [pw, setPw] = useState("");
+  const [count, setCount] = useState(0);
 
   return (
     <div>
-      아이디 :{" "}
-      <input
-        type="text"
-        value={id}
-        onChange={(e) => {
-          setId(e.target.value);
-        }}
-      />{" "}
-      <br />
-      비밀번호 :{" "}
-      <input
-        type="password"
-        value={pw}
-        onChange={(e) => {
-          setPw(e.target.value);
-        }}
-      />
-      <br />
+      <div>{count}</div>
       <button
         onClick={() => {
-          alert(
-            `고객님이 입력하신 아이디는 ${id}이며, 비밀번호는 ${pw}입니다.`
-          );
-          setId("");
-          setPw("");
+          setCount(count - 1);
         }}
       >
-        로그인
+        -1
+      </button>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        +1
       </button>
     </div>
   );
