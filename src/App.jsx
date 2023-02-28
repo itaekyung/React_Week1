68fe2059a3b49ebe50dff701aa0037e7632ddbe3
LeeTaeKyung <leek9708@gmail.com>
2023-02-28 16:46:27 +0900

    jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,18 @@
 import React from "react";
 
-function GrandFather() {
-  return <Mother />;
-}
-
-function Mother() {
-  return <Son />;
-}
-
-function Son() {
-  return <div> 나는 아들입니다.</div>;
-}
-
 function App() {
+  const number = 11;
+  const pTagstyle = { color: "red" }; // 스타일 따로 뺄 수도 있음
   return (
-    <div>
-      <GrandFather />
+    <div className="test-class">
+      <p>안녕하세요 리액트입니다.</p>
+      {/* 주석을 사용하는 방법입니다. */}
+      {/* 삼항 연산자를 사용하는 방법입니다. */}
+      <p style={pTagstyle}>
+        {number > 10 ? number + "은 10보다 크다" : number + "은 10보다 작다"}
+      </p>
     </div>
   );
 }
+
 export default App;
