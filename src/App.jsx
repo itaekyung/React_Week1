0eb48cca9e006ae5d6113ee1c99f43154bfba811
LeeTaeKyung <leek9708@gmail.com>
2023-02-28 18:08:17 +0900

    props2

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 
+//props를 통해 부모 -> 자식 데이터가 전달됐다.
+function Son(props) {
+  return <div>나는 {props.gFatherName}의 손자예요!</div>;
+}
+
+function Mother(props) {
+  const name = "흥부인";
+  const gFatherName = props.grandFatherName;
+  return <Son motherName={name} gFatherName={gFatherName} />; // 자식 컴포넌트에게 정보 전달
+}
+
+function GrandFather() {
+  const name = "나할범";
+  return <Mother grandFatherName={name} />;
+}
+
 function App() {
-  const number = 11;
-  const pTagstyle = { color: "red" }; // 스타일 따로 뺄 수도 있음
-  return (
-    <div className="test-class">
-      <p>안녕하세요 리액트입니다.</p>
-      {/* 주석을 사용하는 방법입니다. */}
-      {/* 삼항 연산자를 사용하는 방법입니다. */}
-      <p style={pTagstyle}>
-        {number > 10 ? number + "은 10보다 크다" : number + "은 10보다 작다"}
-      </p>
-    </div>
-  );
+  return <GrandFather />;
 }
 
 export default App;
