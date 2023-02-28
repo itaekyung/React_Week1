9bc6484318e0be8faa55eac8d6cf8c9bc3607bf1
LeeTaeKyung <leek9708@gmail.com>
2023-02-28 20:04:34 +0900

    input

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,40 @@
 import React, { useState } from "react";
 
 function App() {
-  // hook
-  // useState, useEffect, useContext,
-  const [fruit, setFruit] = useState("");
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
 
   return (
     <div>
-      과일 :{" "}
+      아이디 :{" "}
       <input
-        value={fruit}
-        onChange={(e) => {
-          setFruit(e.target.value);
-        }}
         type="text"
+        value={id}
+        onChange={(e) => {
+          setId(e.target.value);
+        }}
+      />{" "}
+      <br />
+      비밀번호 :{" "}
+      <input
+        type="password"
+        value={pw}
+        onChange={(e) => {
+          setPw(e.target.value);
+        }}
       />
-      {fruit}
+      <br />
+      <button
+        onClick={() => {
+          alert(
+            `고객님이 입력하신 아이디는 ${id}이며, 비밀번호는 ${pw}입니다.`
+          );
+          setId("");
+          setPw("");
+        }}
+      >
+        로그인
+      </button>
     </div>
   );
 }
